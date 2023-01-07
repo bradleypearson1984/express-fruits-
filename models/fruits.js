8b191c1caa4ec532e213627f57e5967eaa9248e9
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-01-07 11:46:01 -0500

    wip: begin process of mvc org

---
+++ models/fruits.js
@@ -0,0 +1,19 @@
+const fruits = [
+    {
+        name: 'apple',
+        color: 'red',
+        readyToEat: false
+    },
+    {
+        name: 'pear',
+        color: 'green',
+        readyToEat: true
+    },
+    {
+        name: 'banana',
+        color: 'yellow',
+        readyToEat: true
+    },
+];
+
+module.exports = fruits;
