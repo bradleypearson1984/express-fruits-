fc46d994f1180801878810cb691c8f8395ad7a9b
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-01-07 11:45:30 -0500

    wip: organized data and render template

--- server.js
+++ server.js
@@ -13,20 +13,33 @@ console.log(process.env);
 const port = process.env.PORT;
 
 //configure database 
-const fruits = ['apple', 'banana', 'pear'];
+const fruits = require('./models/fruits');
+
+
 //mount middleware
 
 //mount routes 
 //INDUCES -mnemonic 
 //index -> GET /fruits  
 app.get('/fruits', function(req, res){
+    const readyToEat = req.query.readyToEat
+    if(readyToEat) {
+        const filteredFruits = fruits.filter(function(f) {
+            return f.readyToEat === (readyToEat === 'true');
+        });
+        res.send(filteredFruits);
+    } else {
+        res.send(fruits);
+    }
     res.send(fruits);
 });
 //show  -> GET /fruits/:someUniqueIdentifier
 app.get('/fruits/:indexOfFruitsArray', function(req, res) {
     const fruit = fruits[req.params.indexOfFruitsArray];
-    res.send(fruit);
+    res.render('show.ejs', { fruit: fruit });
 });
+
+
 //tell the app to listen on a dedicated port for requests 
 app.listen(port, function() {
     console.log(`Express is listening on port:${port}`)
